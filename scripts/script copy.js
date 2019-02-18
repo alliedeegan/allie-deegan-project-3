c10d5b305075d48833075196e9f535fa1701ba56
Allie <alicemdeegan@gmail.com>
2019-02-18 17:20:24 -0500

    responsive, js name-spacing

--- scripts/script copy.js
+++ scripts/script copy.js
@@ -1,13 +1,12 @@
-const colorWiz = {};
 
 // VARIABLES
-colorWiz.counter = 1;
-colorWiz.score = 0;
+let counter = 1;
+let score = 0;
 
-colorWiz.correctAnswerText = ["Nice!", "Take that, Color Wizard!", "Crushin' it!", "You got it!"];
-colorWiz.wrongAnswerText = ["Nope!", "Yiiiikes", "Not even close!", "Your friends are probably doomed", "Hah! No."]
+const correctAnswerText = ["Nice!", "Take that, Color Wizard!", "Crushin' it!", "You got it!"];
+const wrongAnswerText = ["Nope!", "Yiiiikes", "Not even close!", "Your friends are probably doomed", "Hah! No."]
 
-colorWiz.results = {
+const results = {
     amazing: {
         message: "You saved all of your friends! You're the real Color Wizard!",
         img: "assets/crystal.png",
@@ -38,14 +37,14 @@ colorWiz.results = {
 // SMALL FUNCTIONS
 
 //Generates a random number between 0 and 255 
-colorWiz.randNum = function(){
+const randNum = function (){
     const num = Math.floor(Math.random() * 256);
     return num;
 };
 
 
 // I found a tutorial on Campushippo.com for converting rgb to hex. I was struggling with numbers that only returned a single digit until I found the if statement on their site that solved this problem by adding the 0 as a string. So clever!
-colorWiz.singleHexValue = function(rgbValue){
+const singleHexValue = function (rgbValue) {
     let hex = Number(rgbValue).toString(16);
     if (hex.length < 2) {
         hex = "0" + hex;
@@ -54,36 +53,36 @@ colorWiz.singleHexValue = function(rgbValue){
 };
 
 //convert the RGB code to a hex code
-colorWiz.fullHexValue = function(r, g, b){
-    const red = colorWiz.singleHexValue(r);
-    const green = colorWiz.singleHexValue(g);
-    const blue = colorWiz.singleHexValue(b);
+const fullHexValue = function (r, g, b) {
+    const red = singleHexValue(r);
+    const green = singleHexValue(g);
+    const blue = singleHexValue(b);
     return red + green + blue;
 };
 
 //Create a single random hex code
-colorWiz.generateHexCode = function(){
-    return "#" + (colorWiz.fullHexValue(colorWiz.randNum(), colorWiz.randNum(), colorWiz.randNum())).toUpperCase();
+const generateHexCode = function(){
+    return "#" + (fullHexValue(randNum(), randNum(), randNum())).toUpperCase();
 };
 
 // Create an array of three random hex codes
-colorWiz.generateHexCodesArray = function(){
-    return [colorWiz.generateHexCode(), colorWiz.generateHexCode(), colorWiz.generateHexCode()];
+const generateHexCodesArray = function(){
+    return [generateHexCode(), generateHexCode(), generateHexCode()];
 };
 
 //Display the name of the 'correct' hex code
-colorWiz.displayHexName = function(hexcode){
+const displayHexName = function(hexcode){
     $(".question .color-code h2").text(hexcode);
 };
 
 //Determines which correct or inccorrect response user receives 
-colorWiz.getSmallNumber = function(){
+const getSmallNumber = function(){
     const num = Math.floor(Math.random() * 4);
     return num;
 };
 
 //Displays the value of random hex codes
-colorWiz.displayColors = function(colorCodes){
+const displayColors = function (colorCodes) {
     $(".question .option-1").css("background-color", colorCodes[0]).data("id", colorCodes[0]);
     $(".question .option-2").css("background-color", colorCodes[1]).data("id", colorCodes[1]);
     $(".question .option-3").css("background-color", colorCodes[2]).data("id", colorCodes[2]);
@@ -93,29 +92,34 @@ colorWiz.displayColors = function(colorCodes){
 // LARGE FUNCTIONS
 
 //Generates questions
-colorWiz.generateQuestion = function(){
-    let colorCodes = colorWiz.generateHexCodesArray();
+const generateQuestion = function () {
+    let colorCodes = generateHexCodesArray();
     let correctHexCode = colorCodes[0];
-    colorWiz.displayHexName(correctHexCode);
-    colorWiz.checkAnswer(correctHexCode);
+    displayHexName(correctHexCode);
+    console.log(correctHexCode);
+    checkAnswer(correctHexCode);
+    console.log(colorCodes);
     const colorCodesRandom = colorCodes.sort();
-    colorWiz.displayColors(colorCodesRandom);
+    console.log(colorCodesRandom);
+    displayColors(colorCodesRandom);
 };
 
 // Checks the user's answer
-colorWiz.checkAnswer = function(correctHexCode){
-    $(".question .option").bind("click", function(){
+const checkAnswer = function (correctHexCode) {
+    $(".question .option").bind("click", function () {
         $(".question .option").unbind("click");
+        console.log($(this).data("id"));
+        console.log(correctHexCode);
         if ($(this).data("id") === correctHexCode) {
-            $(".question h3").text(`${colorWiz.correctAnswerText[colorWiz.getSmallNumber()]}`);
+            $(".question h3").text(`${correctAnswerText[getSmallNumber()]}`);
             $(this).css("border", "5px solid #55E6C1");
-            colorWiz.score+=1;
+            score+=1;
         } else {
-            $(".question h3").text(`${colorWiz.wrongAnswerText[colorWiz.getSmallNumber()]}`);
+            $(".question h3").text(`${wrongAnswerText[getSmallNumber()]}`);
             $(this).css("border", "5px solid #FC427B");
         }
 
-        if (colorWiz.counter < 10){
+        if (counter < 10){
             $(".next").show();
         } else {
             $(".finish").show();
@@ -125,7 +129,7 @@ colorWiz.checkAnswer = function(correctHexCode){
 
 
 //Handler for all click events
-colorWiz.eventHandler = function(){
+const eventHandler = function(){
     //handler for splash screen
     $("button.splash").on("click", function(){
         $("h1.splash").removeClass("active");
@@ -149,7 +153,7 @@ colorWiz.eventHandler = function(){
     });
 
     //handler for how-to navigation forward
-    $("body").on("click", ".next-how-to", function(){
+    $("body").on("click", ".next-how-to", function () {
         const currentPartId = `*[data-part="${$('.active-part').attr('data-part')}"]`;
         const nextPartId = parseInt($('.active-part').attr('data-part')) + 1;
         const nextPart = `*[data-part="${nextPartId}"]`;
@@ -158,7 +162,7 @@ colorWiz.eventHandler = function(){
     });
 
     //hander for how-to navigation backward
-    $("body").on("click", ".last-how-to", function(){
+    $("body").on("click", ".last-how-to", function () {
         const currentPartId = `*[data-part="${$('.active-part').attr('data-part')}"]`;
         const lastPartId = parseInt($('.active-part').attr('data-part')) - 1;
         const lastPart = `*[data-part="${lastPartId}"]`;
@@ -178,13 +182,13 @@ colorWiz.eventHandler = function(){
     });
 
     //handler for next question
-    $(".next").on("click", function(){
+    $(".next").on("click", function () {
         $(this).hide();
         $(".option").css("border", "5px solid #fff");
         $(".question h3").text(" ");
-        colorWiz.counter+=1;
-        if (colorWiz.counter <=10){
-            colorWiz.generateQuestion();
+        counter+=1;
+        if (counter <=10){
+            generateQuestion();
         }
     });
     
@@ -192,45 +196,45 @@ colorWiz.eventHandler = function(){
     $(".finish").on("click", function(){
         $("section.question").hide();
         $("section.result").show();
-        colorWiz.showResult();
+        showResult();
     }); 
 };
 
 
 // Show's the User's score and result screen
-colorWiz.showResult = function(){
-    $(".user-score").text(colorWiz.score);
-    if (colorWiz.score === 10){
-        $(".result-text p").text(colorWiz.results.amazing.message);
-        $(".result-text img").attr("src", `${colorWiz.results.amazing.img}`);
-        $(".result-text img").attr("alt", `${colorWiz.results.amazing.alt}`);
-    } else if (colorWiz.score < 10 && colorWiz.score >= 8){
-        $(".result-text p").text(colorWiz.results.good.message);
-        $(".result-text img").attr("src", `${colorWiz.results.good.img}`);
-        $(".result-text img").attr("alt", `${colorWiz.results.good.alt}`);
-    } else if (colorWiz.score < 8 && colorWiz.score >= 6){
-        $(".result-text p").text(colorWiz.results.fair.message);
-        $(".result-text img").attr("src", `${colorWiz.results.fair.img}`);
-        $(".result-text img").attr("alt", `${colorWiz.results.fair.alt}`);
-    } else if (colorWiz.score < 6 && colorWiz.score >= 3){
-        $(".result-text p").text(colorWiz.results.poor.message);
-        $(".result-text img").attr("src", `${colorWiz.results.poor.img}`);
-        $(".result-text img").attr("alt", `${colorWiz.results.poor.alt}`);
+const showResult = function(){
+    $(".user-score").text(score);
+    if (score === 10){
+        $(".result-text p").text(results.amazing.message);
+        $(".result-text img").attr("src", `${results.amazing.img}`);
+        $(".result-text img").attr("alt", `${results.amazing.alt}`);
+    } else if (score < 10 && score >= 8){
+        $(".result-text p").text(results.good.message);
+        $(".result-text img").attr("src", `${results.good.img}`);
+        $(".result-text img").attr("alt", `${results.good.alt}`);
+    } else if (score < 8 && score >= 6){
+        $(".result-text p").text(results.fair.message);
+        $(".result-text img").attr("src", `${results.fair.img}`);
+        $(".result-text img").attr("alt", `${results.fair.alt}`);
+    } else if (score < 6 && score >= 3){
+        $(".result-text p").text(results.poor.message);
+        $(".result-text img").attr("src", `${results.poor.img}`);
+        $(".result-text img").attr("alt", `${results.poor.alt}`);
     } else {
-        $(".result-text p").text(colorWiz.results.bad.message);
-        $(".result-text img").attr("src", `${colorWiz.results.bad.img}`);
-        $(".result-text img").attr("alt", `${colorWiz.results.bad.alt}`);
+        $(".result-text p").text(results.bad.message);
+        $(".result-text img").attr("src", `${results.bad.img}`);
+        $(".result-text img").attr("alt", `${results.bad.alt}`);
     }
 };
 
 
-colorWiz.init = function(){
+const init = function(){
     particlesJS.load('particles-js', 'scripts/particles.json');
-    colorWiz.eventHandler();
-    colorWiz.generateQuestion();
+    eventHandler();
+    generateQuestion();
 };
 
 
 $(document).ready(function(){
-    colorWiz.init();
+    init();
 });
